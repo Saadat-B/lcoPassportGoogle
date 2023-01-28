df274c2093f00019e4b90ccb401c33146aea007f
Saadat Badgujar <saadatbadgujar@gmail.com>
2023-01-28 11:39:34 +0530

    Serialize and Deserialize user

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
-
 const auth = require("./routes/auth");
 const passportConfig = require("./passport/passport");
 const passport = require("passport");
+const cookieSession = require("cookie-session");
+const app = express();
+
 mongoose.connect("mongodb://127.0.0.1:27017/passport", () => {
   console.log("DB CONNECTED");
 });
 
+app.use(
+  cookieSession({
+    maxAge: 3 * 24 * 60 * 60 * 1000,
+    keys: ["thisislcotokenkey"],
+  })
+);
+
 app.use(passport.initialize());
+app.use(passport.session());
 
 app.set("view engine", "ejs");
 app.use("/auth", auth);
