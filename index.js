060425ecf824fb4bb6f218fd0513ec92bde5cb65
Saadat Badgujar <saadatbadgujar@gmail.com>
2023-01-28 10:47:21 +0530

    Showing consent screen of google

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 const auth = require("./routes/auth");
+require("./passport/passport");
 
 mongoose.connect("mongodb://127.0.0.1:27017/passport", () => {
   console.log("DB CONNECTED");
---
+++ passport/passport.js
@@ -0,0 +1,17 @@
+const passport = require("passport");
+
+var GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "147053614689-6g39grqum8l313imdon4shmn0t481ofu.apps.googleusercontent.com",
+      clientSecret: "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",
+      callbackURL: "http://localhost:4000/auth/google/callback",
+    },
+    () => {
+      //callback
+    }
+  )
+);
